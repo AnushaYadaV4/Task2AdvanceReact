ec34dd1c721c2c7e3dacb596d2273b55e5d8fad6
AnushaYadaV4 <anu468mjl@gmail.com>
2022-11-27 18:08:24 +0530

    fourth commit

---
+++ ExpenseItem.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
+
+import './ExpenseItem.css';
+
+
+const ExpenseItem=(props)=>{
+    const clikHandler=()=>{
+        console.log("Clicked!!!!!")
+    }
+   
+    
+    return (
+        <Card className='expense-item'>
+            <ExpenseDate date ={props.date} />
+            <div className='expense-item__description'>
+                <h2>{props.title}</h2>
+                <div className='expense-item__price'>{props.amount}</div>
+            </div>
+            <button onClick={clikHandler}>Change title</button>
+            <button onClick={clikHandler}>Delete Expense</button>
+
+        </Card>
+    );
+}
+
+export default ExpenseItem;
